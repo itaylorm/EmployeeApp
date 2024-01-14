9fe6a4ac8b1979ad4cb09e60740fc63eaf9f3941
Taylor Maxwell <taylor@imaxwell.net>
2024-01-14 13:21:53 -0800

    Added text to save buttons

---
+++ iMaxwell.EmployeeClient/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using iMaxwell.Common.Models;
+
+namespace iMaxwell.EmployeeClient.Models
+{
+    public class Department : IDepartmentModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} Name:{Name}";
+        }
+    }
+}
